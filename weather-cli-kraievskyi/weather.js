47e9bfdd7fc415f6699e796547ec03511cededbf
rambutan <pride.kraievskyi@gmail.com>
2022-09-26 18:23:29 +0300

    add weatherApi

--- weather-cli-kraievskyi/weather.js
+++ weather-cli-kraievskyi/weather.js
@@ -1,8 +1,22 @@
 #!/usr/bin/env node
 
 import { getArgs } from './helpers/args.js';
-import { printHelp } from './services/log.service.js'
-import { saveKeyValue } from './services/storage.service.js'
+import { printError, printHelp, printSuccess } from './services/log.service.js'
+import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js'
+import { getWeather } from './services/api.service.js'
+
+const saveToken = async (token) => {
+	if(!token.length){
+		printError('Токен не встановлено');
+		return;
+	}
+	try {
+		await saveKeyValue(TOKEN_DICTIONARY.token, token);
+		printSuccess('Токен збережено')
+	} catch (e) {
+		printError(e.message);
+	}
+}
 
 const initCLI = () => {
 	const args = getArgs(process.argv);
@@ -13,8 +27,10 @@ const initCLI = () => {
 
 	}
 	if (args.t) {
-		saveKeyValue('token', args.t);
+		return saveToken(args.t)
 	}
+	getWeather('moscow');
 };
 
 initCLI();
+
