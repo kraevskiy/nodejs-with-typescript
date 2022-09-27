4eb3bf6e924364073248019f6518ec8d7def1dad
rambutan <pride.kraievskyi@gmail.com>
2022-09-27 15:16:59 +0300

    dashboard-api - add LoggerService

--- dashboard-api/src/app.ts
+++ dashboard-api/src/app.ts
@@ -1,14 +1,17 @@
 import express, { Express } from 'express';
 import { Server } from 'http';
+import { LoggerService } from './logger/logger.service';
 
 export class App {
 	app: Express;
 	port: number;
 	server: Server;
+	logger: LoggerService;
 
-	constructor() {
+	constructor(logger: LoggerService) {
 		this.app = express();
 		this.port = 8000;
+		this.logger = logger;
 	}
 
 	useRoutes() {
@@ -18,6 +21,7 @@ export class App {
 	public async init() {
 		this.useRoutes();
 		this.server = this.app.listen(this.port, () => {
+			this.logger.log(`Server start http://localhost:${this.port}`);
 			console.log(`Server start http://localhost:${this.port}`);
 		})
 	}
